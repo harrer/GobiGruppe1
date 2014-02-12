c09361b506b66cb6f02936378d7159f1f85a897d
carstenuhlig <carsten.uhlig@gmail.com>
2014-02-12 12:24:03 +0100

    .project File

---
+++ src/de/lmu/ifi/bio/splicing/genome/AbstractExon.java
@@ -0,0 +1,8 @@
+package de.lmu.ifi.bio.splicing.genome;
+
+public class AbstractExon extends de.lmu.ifi.bio.splicing.interfaces.AbstractExon {
+
+    AbstractExon(long start, long stop, int frame) {
+        super(start, stop, frame);
+    }
+}
