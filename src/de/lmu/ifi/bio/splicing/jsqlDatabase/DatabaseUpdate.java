c2829e13d9b6c14bd431213df18cba0c0f850545
harrer <tobiasharrer@gmail.com>
2014-02-12 11:54:57 +0100

    util zkoss gelöscht

---
+++ src/de/lmu/ifi/bio/splicing/jsqlDatabase/DatabaseUpdate.java
@@ -0,0 +1,5 @@
+package de.lmu.ifi.bio.splicing.jsqlDatabase;
+
+public interface DatabaseUpdate {
+
+}
--- src/de/lmu/ifi/bio/splicing/zkoss/service/StubClass.java
+++
@@ -1,7 +0,0 @@
-package de.lmu.ifi.bio.splicing.zkoss.service;
-
-/**
- * Created by uhligc on 11.02.14.
- */
-public class StubClass {
-}
