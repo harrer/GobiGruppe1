f235a79871b6beb876422f055c33a5a5882e19b6
TheSeoman <seoman@web.de>
2014-02-12 19:44:20 +0100

    EventAnnotation Intellij Init

--- src/de/lmu/ifi/bio/splicing/eventdetection/EventDetector.java
+++ src/de/lmu/ifi/bio/splicing/eventdetection/EventDetector.java
@@ -1,7 +1,9 @@
 package de.lmu.ifi.bio.splicing.eventdetection;
 
 import de.lmu.ifi.bio.splicing.genome.*;
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class EventDetector {
@@ -9,6 +11,11 @@ public class EventDetector {
 	public static Set<Event> getEvents(Transcript t1, Transcript t2, boolean strand){
 		EventAnotation ea = new EventAnotation(t1, t2, strand);
 		return makeEventSet(ea);
+	}
+	
+	public static List<long[]> getEventList(Transcript t1, Transcript t2, boolean strand){
+		EventAnotation ea = new EventAnotation(t1, t2, strand);
+		return ea.getEventsP();
 	}
 	
 	public static Set<Event> makeEventSet(EventAnotation ea){
@@ -27,4 +34,70 @@ public class EventDetector {
 		}
 		return events;
 	}
+	
+	public static void printEvents(EventAnotation ea) {
+		System.out.println("Conserved:");
+		for (long[] e : ea.getEventsN()) {
+			if (e[0] == 0)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+		System.out.println("\nDeletions:");
+		for (long[] e : ea.getEventsN()) {
+			if (e[0] == 1)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+		System.out.println("\nInserts");
+		for (long[] e : ea.getEventsN()) {
+			if (e[0] == 2)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+
+		System.out.println("\nConserved:");
+		for (long[] e : ea.getEventsP()) {
+			if (e[0] == 0)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+		System.out.println("\nDeletions:");
+		for (long[] e : ea.getEventsP()) {
+			if (e[0] == 1)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+		System.out.println("\nInserts");
+		for (long[] e : ea.getEventsP()) {
+			if (e[0] == 2)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+		System.out.println("\nReplaces");
+		for (long[] e : ea.getEventsP()) {
+			if (e[0] == 3)
+				System.out.print("(" + e[1] + ", " + e[2] + ") ");
+		}
+		System.out.println();
+	}
+	
+	public void printVarSplice(EventAnotation ea) {
+		StringBuilder sbp = new StringBuilder();
+		for (long[] a : ea.getEventsP()) {
+			if (a[0] == 0) {
+				for (long i = a[1]; i <= a[2]; i++) {
+					sbp.append("*");
+				}
+			} else if (a[0] == 1) {
+				for (long i = a[1]; i <= a[2]; i++) {
+					sbp.append("D");
+				}
+			} else if (a[0] == 2) {
+				for (long i = a[1]; i <= a[2]; i++) {
+					sbp.append("I");
+				}
+			} else if (a[0] == 3) {
+				for (long i = a[1]; i <= a[2]; i++) {
+					sbp.append("R");
+				}
+			}
+		}
+		// System.out.println(sbp.toString().replaceAll("(.)", "$1  "));
+		System.out.println(sbp);
+	}
+
 }
