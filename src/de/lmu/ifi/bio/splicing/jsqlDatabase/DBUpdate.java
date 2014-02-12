e9f6cff4bd2adfda6a33eadc10335238b3369831
carstenuhlig <carsten.uhlig@gmail.com>
2014-02-12 15:12:24 +0100

    DatabaseAnfragen

---
+++ src/de/lmu/ifi/bio/splicing/jsqlDatabase/DBUpdate.java
@@ -0,0 +1,30 @@
+package de.lmu.ifi.bio.splicing.jsqlDatabase;
+
+import de.lmu.ifi.bio.splicing.genome.Event;
+import de.lmu.ifi.bio.splicing.genome.Gene;
+import de.lmu.ifi.bio.splicing.genome.Transcript;
+
+/**
+ * Created by uhligc on 12.02.14.
+ */
+public class DBUpdate implements DatabaseUpdate {
+    @Override
+    public void insertGene(Gene gene) {
+
+    }
+
+    @Override
+    public void insertTranscript(Transcript transcript) {
+
+    }
+
+    @Override
+    public void insertEvent(Event event, Transcript transcript) {
+
+    }
+
+    @Override
+    public void insertEvent(Event event) {
+
+    }
+}
