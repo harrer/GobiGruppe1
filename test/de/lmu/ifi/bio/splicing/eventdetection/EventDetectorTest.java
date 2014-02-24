792cc354bbbb79aba0c86d5956953e1e2440d8a1
TheSeoman <seoman@web.de>
2014-02-24 09:25:23 +0100

    DBUpdate complete

--- test/de/lmu/ifi/bio/splicing/eventdetection/EventDetectorTest.java
+++ test/de/lmu/ifi/bio/splicing/eventdetection/EventDetectorTest.java
@@ -15,8 +15,8 @@ public class EventDetectorTest extends TestCase {
 
     public void setUp() throws Exception {
         super.setUp();
-        Transcript p1 = Setting.dbq.getTranscript("ENST00000557594");
-        Transcript p2 = Setting.dbq.getTranscript("ENST00000335750");
+        Transcript p1 = Setting.dbq.getTranscript("ENST00000455878");
+        Transcript p2 = Setting.dbq.getTranscript("ENST00000157812");
         ea = new EventAnnotation(p1, p2, true);
     }
 
